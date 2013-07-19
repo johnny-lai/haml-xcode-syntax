746fb047f3be213078854cb5f5c9aa7ac2e30c98
Johnny Lai <johnny.lai@me.com>
2013-07-20 06:29:03 +0800

    Updated to Haml. Haml syntax is now a copy of the Ruby syntax

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# Lua Support for Xcode 4+ #
-1. download the two files (Add-Lua.sh, Lua.xclangspec)
-2. Open Add-Lua.sh and change the DVTFOUNDATION_PATH if necessary.  You probably do not need to change this if you are on Xcode 4.3+
-3. Make sure Xcode is closed
-4. Run Add-Lua.sh from the terminal with sudo (I.E. 'sudo Add-Lua.sh')
-5. Enter your admin password and hit enter
-6. Open Xcode and notice the 'Lua' entry under "Editor->Syntax Coloring"
+*Note: The current syntax highlighting is just a copy of the Ruby highlighting*
 
-
-Special thanks to [araxara](https://github.com/araxara) for the Objective-J version of this code.
+# HAML Support for Xcode 4+ #
+1. Download the files (install.sh, Haml.xclangspec, Haml.plist)
+2. Make sure Xcode is closed
+3. Run install.sh from the terminal
+   You will get a confirmation with your current Xcode installation path.
+4. Press Enter to continue. Otherwise use Ctrl+C to quit.
+5. Open Xcode and notice the 'Lua' entry under "Editor->Syntax Coloring"
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#  Created by Alex Karahalios on 6/12/11
+#  Edited by Brian Reinhart on 08/02/2012 .
+#  Last edited 8/02/2012
+#
+# Updates Xcode and to support LUA language for editing
+#
+
+# Path were this script is located
+#
+SCRIPT_PATH="$(dirname "$BASH_SOURCE")"
+
+# Set up path for PlistBuddy helper application which can add elements to Plist files
+#
+PLISTBUDDY=/usr/libexec/PlistBuddy
+
+# Xcode Contents Path
+#
+XCODE_CONTENTS_PATH="$(dirname "`xcode-select -p`")"
+
+# This framework is found withing the Xcode.app package and is used when Xcode is a monolithic
+# install (all contained in Xcode.app)
+#
+DVTFOUNDATION_PATH="$XCODE_CONTENTS_PATH/SharedFrameworks/DVTFoundation.framework/Versions/A/Resources/"
+
+# Wait for user confirmation
+#
+echo "*** IMPORTANT! This package does not yet actually include the correct Haml syntax highlighting yet ***"
+echo "About to patch: $DVTFOUNDATION_PATH"
+read -p "Press [Enter] to continue"
+
+# Now merge in the additonal languages to DVTFoundation.xcplugindata
+#
+$PLISTBUDDY "$DVTFOUNDATION_PATH/DVTFoundation.xcplugindata"  -c 'Merge Haml.plist plug-in:extensions'
+
+# Copy in the xclangspecs for the languages (assumes in same directory as this shell script)
+#
+cp "$SCRIPT_PATH/Haml.xclangspec" "$DVTFOUNDATION_PATH"
+
+# Remove any cached Xcode plugins
+#
+rm -f /private/var/folders/*/*/*/com.apple.DeveloperTools/*/Xcode/PlugInCache.xcplugincache
